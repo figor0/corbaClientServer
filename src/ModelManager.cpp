7738012fdee4da7daf31b54eb2e548c759136f85
Figoro <furinforjob@gmail.com>
2020-09-13 16:02:30 +0300

    ModelManager: throw std::invalid_argiment if a model are not seted

--- src/ModelManager.cpp
+++ src/ModelManager.cpp
@@ -46,12 +46,16 @@ void ModelManager::registration(QQmlContext* context_ptr,
 								const QString &model_name,
 								const QString &model_manager_name)
 {
+	if (m_model_ptr == nullptr)
+		throw std::invalid_argument("Model are not exitst");
 	context_ptr->setContextProperty(model_name, m_model_ptr.get());
 	context_ptr->setContextProperty(model_manager_name, this);
 }
 
 void ModelManager::resetData(const ModelManager::Sequence &sequence)
 {
+	if (m_model_ptr == nullptr)
+		throw std::invalid_argument("Model are not exitst");
 	if (m_model_ptr->rowCount() != sequence.length()){
 		m_model_ptr->removeRows(0, m_model_ptr->rowCount(), QModelIndex());
 		m_model_ptr->insertRows(0, sequence.length(), QModelIndex());
@@ -64,6 +68,8 @@ void ModelManager::resetData(const ModelManager::Sequence &sequence)
 
 bool ModelManager::setEntry(const size_t index, const EntryIdl& entry)
 {
+	if (m_model_ptr == nullptr)
+		throw std::invalid_argument("Model are not exitst");
 	bool right = false;
 	right = m_model_ptr->setData(m_model_ptr->index(index, 0),
 						 QVariant(entry.first_name), PhoneEntryRoles::FirstName);
@@ -78,6 +84,8 @@ bool ModelManager::setEntry(const size_t index, const EntryIdl& entry)
 
 EntryIdl ModelManager::getEntry(const size_t row_number) const
 {
+	if (m_model_ptr == nullptr)
+		throw std::invalid_argument("Model are not exitst");
 	return {
 		m_model_ptr->data(m_model_ptr->index(row_number, 0),
 							PhoneEntryRoles::FirstName).toString().toStdString().data(),
@@ -92,6 +100,8 @@ EntryIdl ModelManager::getEntry(const size_t row_number) const
 
 ModelManager::Sequence ModelManager::getEntries() const
 {
+	if (m_model_ptr == nullptr)
+		throw std::invalid_argument("Model are not exitst");
 	Sequence entries;
 	entries.length(m_model_ptr->rowCount());
 	for (size_t i = 0; i < entries.length(); i++)
