53725c96122fad547e806db8c8d55f465668c355
Figoro <furinforjob@gmail.com>
2020-09-13 18:31:15 +0300

    move registration from ModelManager to client code

--- client.cpp
+++ client.cpp
@@ -12,19 +12,30 @@
 #include <ModelManager.h>
 #include <APhonesModelManager.h>
 
+void registration(APhonesModel* model_ptr,
+				  const QString& model_name,
+				  APhonesModelManager* manager_ptr,
+				  const QString& manager_name,
+				  QQmlContext* context_ptr)
+{
+	context_ptr->setContextProperty(model_name, model_ptr);
+	context_ptr->setContextProperty(manager_name, manager_ptr);
+}
+
 int main(int argc, char* argv[])
 {
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QGuiApplication app(argc, argv);
 	QQmlApplicationEngine engine;
-
 	CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
 	auto corba_loader_ptr = std::make_shared<CorbaLoader>(orb);
 	ModelManager model_manager(corba_loader_ptr);
 	std::shared_ptr<APhonesModel> model_ptr(new EntriesModel);
 	model_manager.setModel(model_ptr);
-	model_manager.registration(engine.rootContext(), "entries", "entries_manager");
-
+	registration(model_ptr.get(),
+				 "entries", &model_manager,
+				 "entries_manager",
+				 engine.rootContext());
 	const QUrl url(QStringLiteral("qrc:/main.qml"));
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 					 &app, [url](QObject *obj, const QUrl &objUrl) {
